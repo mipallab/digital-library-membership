f12056b8a1f2b88795311c742f375757a7451a3b
mipallab <pallab@qmail.com.bd>
2026-08-06 00:16:32 +0600

    bump version to 1.9.12

--- digital-library-membership.php
+++ digital-library-membership.php
@@ -15,7 +15,7 @@
  * Plugin Name:       Digital Library Membership
  * Plugin URI:        https://profiles.wordpress.org/mipallab123/digital-library-membership
  * Description:       A premium, secure subscription membership plugin to read digital books frontend with a physical page-flip feel.
- * Version:           1.9.11
+ * Version:           1.9.12
  * Author:            Majadul Islam Pallab
  * Author URI:        https://profiles.wordpress.org/mipallab123
  * License:           GPL-2.0+
@@ -34,7 +34,7 @@ if ( ! defined( 'WPINC' ) ) {
 /**
  * Define Plugin Constants
  */
-define( 'DLM_VERSION', '1.9.11' );
+define( 'DLM_VERSION', '1.9.12' );
 define( 'DLM_PATH', plugin_dir_path( __FILE__ ) );
 define( 'DLM_URL', plugin_dir_url( __FILE__ ) );
 
